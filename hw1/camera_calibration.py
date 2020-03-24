fb45e07bf77a26a6012d567ab5ed63f5ea11f183
AegeanKI <pmes.986612@gmail.com>
2020-03-24 15:46:40 +0800

    try to solve B = K^{-T}K^{-1}

--- hw1/camera_calibration.py
+++ hw1/camera_calibration.py
@@ -25,11 +25,14 @@ print('Start finding chessboard corners...')
 for idx, fname in enumerate(images):
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    plt.imshow(gray)
+#     plt.imshow(gray)
 
     #Find the chessboard corners
     print('find the chessboard corners of',fname)
     ret, corners = cv2.findChessboardCorners(gray, (corner_x,corner_y), None)
+    # shape(corners) = (49, 1, 2)
+#     corners = corners.reshape(49,2)
+    corners = np.flip(np.flip(corners.reshape(49,2), 0).reshape(7,7,2).transpose(1,0,2), 0).reshape(49,2)
 
     # If found, add object points, image points
     if ret == True:
@@ -38,9 +41,7 @@ for idx, fname in enumerate(images):
 
         # Draw and display the corners
         cv2.drawChessboardCorners(img, (corner_x,corner_y), corners, ret)
-        plt.imshow(img)
-
-
+#         plt.imshow(img)
 #######################################################################################################
 #                                Homework 1 Camera Calibration                                        #
 #               You need to implement camera calibration(02-camera p.76-80) here.                     #
@@ -61,11 +62,87 @@ Tr = np.array(tvecs)
 extrinsics = np.concatenate((Vr, Tr), axis=1).reshape(-1,6)
 """
 Write your code here
-
-
-
-
 """
+def get_v(H, i, j):
+#     return np.array([H[0][i]*H[0][j],
+#                     H[0][i]*H[1][j]+H[1][i]*H[0][j],
+#                     H[1][i]*H[1][j],
+#                     H[2][i]*H[0][j]+H[0][i]*H[2][j],
+#                     H[2][i]*H[1][j]+H[1][i]*H[2][j],
+#                     H[2][i]*H[2][j]])
+    return np.array([H[i][0]*H[j][0],
+                    H[i][0]*H[j][1]+H[i][1]*H[j][0],
+                    H[i][1]*H[j][1],
+                    H[i][2]*H[j][0]+H[i][0]*H[j][2],
+                    H[i][2]*H[j][1]+H[i][1]*H[j][2],
+                    H[i][2]*H[j][2]])
+
+V = []
+for i in range(np.shape(objpoints)[0]):
+    print(i)
+    objmatrix = objpoints[i]
+    objmatrix[:,-1] = np.ones(corner_x*corner_y, np.float32)
+    print(objmatrix.shape)
+    
+    imgmatrix = np.ones((corner_x*corner_y, 3), np.float32)
+    imgmatrix[:,:-1] = imgpoints[i]
+    print(imgmatrix.shape)
+
+    # 0, 1, 2 use (no T and down) or (T and up)  can let u5 do cholesky
+    # 0, 1, 2, 3 use (no T and up) or (T and down) can let u4 do cholesky
+    H = np.linalg.inv(objmatrix.T.dot(objmatrix)).dot(objmatrix.T).dot(imgmatrix).T
+
+#     # normalize
+#     lengths = np.linalg.norm(H, axis=-1)
+#     H[lengths > 0] = H[lengths > 0] / lengths[lengths > 0][:, np.newaxis]
+    print(H)
+    v12 = get_v(H, 0, 1)
+    v11 = get_v(H, 0, 0)
+    v22 = get_v(H, 1, 1)
+    V.append(v12)
+    V.append(v11-v12)
+    
+
+
+
+V = np.array(V)
+print(np.shape(V))
+u, s, vh = np.linalg.svd(V.dot(V))
+# print("u, s, vh")
+print(u)
+print(s)
+# min_eigenvalue_index = list(s).index(min(s))
+# b = u[min_eigenvalue_index]
+
+for i in range(0, s.size):
+    b = u[i]
+    print(b)
+    B = np.array([[b[0], b[1], b[3]],
+                [b[1], b[2], b[4]],
+                [b[3], b[4], b[5]]])
+    try:
+        L = np.linalg.cholesky(B)
+        print("u{}.can do cholesky".format(i))
+    except:
+        print("u{} failed".format(i))
+
+for i in range(0, s.size):
+    b = vh[i]
+    print(b)
+    B = np.array([[b[0], b[1], b[3]],
+                [b[1], b[2], b[4]],
+                [b[3], b[4], b[5]]])
+    try:
+        L = np.linalg.cholesky(B)
+        print("vh{}.can do cholesky".format(i))
+    except:
+        print("vh{} failed".format(i))
+raise SystemExit(0)
+
+
+
+
+
 # show the camera extrinsics
 print('Show the camera extrinsics')
 # plot setting
@@ -107,7 +184,7 @@ ax.set_xlabel('x')
 ax.set_ylabel('z')
 ax.set_zlabel('-y')
 ax.set_title('Extrinsic Parameters Visualization')
-plt.show()
+# plt.show()
 
 #animation for rotating plot
 """
