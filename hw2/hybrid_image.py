e54f6b239f2f4ab2278317ec9181759dab0a29c6
AegeanKI <pmes.986612@gmail.com>
2020-04-11 23:42:14 +0800

    try to hybrid colored image, but failed

--- hw2/hybrid_image.py
+++ hw2/hybrid_image.py
@@ -9,47 +9,63 @@ def make_gaussian(row, col, sigma, is_high):
     res = np.zeros((row, col))
     for i in range(row):
         for j in range(col):
-            val = np.exp(((i - row // 2) ** 2 + (j - col // 2) ** 2) / (-2 * sigma ** 2))
+            val = np.exp(-1 * ((i - row // 2) ** 2 + (j - col // 2) ** 2) / (2 * (sigma ** 2)))
             if is_high:
                 val = 1 - val
             res[i][j] = val
     return res
 
-def freq_filter(img, sigma, is_high):
-#     for i in range(img.shape[0]):
-#         for j in range(img.shape[1]):
-#             img_copy[i][j] *= pow(-1, i+j)
+def move_center(img):
+    row = img.shape[0]
+    col = img.shape[1]
+    img_copy = np.array(img)
+    for i in range(row):
+        for j in range(col):
+            img_copy[i][j] *= pow(-1, i+j)
 
+    return img_copy
+    
+
+def filt(img, freq_pass_func):
     img_freq = fftshift(fft2(img))
-    freq_pass_func = make_gaussian(img.shape[0], img.shape[1], sigma, is_high)
-    img_freq_filtered = img_freq * freq_pass_func
-    img_filtered = ifft2(ifftshift(img_freq_filtered)).real
+#     img_freq = fft2(img)
+#     freq_pass_func = fftshift(fft2(freq_pass_func))
+    img_freq_filted = img_freq * freq_pass_func
+    img_filted = ifft2(ifftshift(img_freq_filted)).real
+#     img_filted = ifft2(img_freq_filted).real
+    return img_filted
 
-#     for i in range(img.shape[0]):
-#         for j in range(img.shape[1]):
-#             img_filtered[i][j] *= pow(-1, i+j)
-    return img_filtered
+def freq_filter(img, sigma, is_high):
+    row = img.shape[0]
+    col = img.shape[1]
+    img_rgb = [i.reshape(row, col) for i in np.dsplit(img, 3)]
+    img_rgb = [move_center(i) for i in img_rgb]
 
+    freq_pass_func = make_gaussian(row, col, sigma, is_high)
+    img_filted_rgb = [filt(i, freq_pass_func) for i in img_rgb]
+
+    img_filted_rgb = [move_center(i) for i in img_filted_rgb]
+    img_filted = np.stack(img_filted_rgb, axis=2)
+    return img_filted
 
 
 def hybrid(img_high, img_low, sigma_high, sigma_low):
     img_high_pass = freq_filter(img_high, sigma_high, is_high=True)
-    img_low_pass = freq_filter(img_low, sigma_low, is_high=False)
+#     img_low_pass = freq_filter(img_low, sigma_low, is_high=False)
 
     return img_high_pass + img_low_pass
 
 def get_img(img_name):
-    img_rgb = imread(IMAGE_DIR + img_name).astype('float')
-    img_gray = np.dot(img_rgb[...,:3], [0.299, 0.587, 0.114])
-    return img_gray
+    img_rgb = imread(IMAGE_DIR + img_name).astype('int')
+    return img_rgb
 
 
 if __name__ == "__main__":
-    img_high = get_img('4_einstein.bmp')
-    img_low = get_img('4_marilyn.bmp')
+    img_low = get_img('4_einstein.bmp')
+    img_high = get_img('4_marilyn.bmp')
     
-    img_hybrid = hybrid(img_high, img_low, sigma_high=25, sigma_low=10)
-    plt.imshow(img_hybrid, cmap='gray', vmin=0, vmax=255)
+    img_hybrid = hybrid(img_high, img_low, sigma_high=2, sigma_low=1)
+    plt.imshow(img_hybrid)
 
 #     plt.show()
     plt.draw()
