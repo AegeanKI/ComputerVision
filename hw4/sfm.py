8d7d4abb83a44c531e166ab7e34b3592f9640c51
AegeanKI <pmes.986612@gmail.com>
2020-06-03 16:01:19 +0800

    try to fix reconstruct

--- hw4/sfm.py
+++ hw4/sfm.py
@@ -99,19 +99,8 @@ def find_img_keypoint(img):
 ##################### Compute RANSAC (fundamental) ###############################
 
 def geometricDistance(p0, p1, f, shape):
+    disparity = abs(p1.reshape(3,1).T @ f @ p0.reshape(3,1))[0,0]
     disparity = abs(p1.T @ f @ p0)
-    return disparity
-
-    extend_p0 = np.array([p0[0], p0[1], 1])
-    # estimate_p1 = np.dot(h, extend_p0)
-    # estimate_p1 = estimate_p1 / estimate_p1[-1]
-    
-    extend_p1 = np.array([p1[0], p1[1], 1])
-    T1 = np.array([[2/shape[1], 0, -1], [0, 2/shape[0], -1], [0, 0, 1]])
-    extend_p1 = np.dot(T1, extend_p1.T)
-    extend_p0 = np.dot(T1, extend_p0.T)
-    disparity = abs((extend_p1.T @ f) @ extend_p0)
-    # return np.linalg.norm(error)
     return disparity
 
 def append_p(P, obj, img):
@@ -127,74 +116,14 @@ def compute_fundamental(x1,x2):
     for i in range(len(x1)):
         arr.append(GetRowOf_F(x1[i], x2[i]))
 
-    U, S, V = np.linalg.svd(np.array(arr))
-    F = np.reshape(V[-1], (3, 3))
+    U, S, VH = np.linalg.svd(np.array(arr))
+    F = np.reshape(VH[-1], (3, 3))
     
-    U, D, V = np.linalg.svd(F)
+    U, D, VH = np.linalg.svd(F)
     D[2] = 0
-    F = U @ np.diag(D) @ V
+    F = U @ np.diag(D) @ VH
     F /= F[-1, -1]
 
-    return F
-#     """
-#     Computes the fundamental matrix from corresponding points 
-#     (x1,x2 3*n arrays) using the 8 point algorithm.
-#     Each row in the A matrix below is constructed as
-#     [x'*x, x'*y, x', y'*x, y'*y, y', x, y, 1]
-#     """    
-#     n = x1.shape[1]
-#        
-#     # build matrix for equations
-#     A = np.zeros((n,9))
-#     for i in range(n):
-#         A[i] = [x1[0,i]*x2[0,i], x1[0,i]*x2[1,i], x1[0,i]*x2[2,i],
-#                 x1[1,i]*x2[0,i], x1[1,i]*x2[1,i], x1[1,i]*x2[2,i],
-#                 x1[2,i]*x2[0,i], x1[2,i]*x2[1,i], x1[2,i]*x2[2,i] ]
-#             
-#     # compute linear least square solution
-#     U,S,V = np.linalg.svd(A)
-#     F = V[-1].reshape(3,3)
-#         
-#     # constrain F
-#     # make rank 2 by zeroing out last singular value
-#     U,S,V = np.linalg.svd(F)
-#     S[2] = 0
-#     F = np.dot(U,np.dot(np.diag(S),V))
-# 
-#     return F/F[2,2]
-#     return F
-
-
-def compute_fundamental_normalized(x1, x2, shape):
-    """
-    Computes the fundamental matrix from corresponding points 
-    (x1,x2 3*n arrays) using the normalized 8 point algorithm. 
-    """
-    # n = x1.shape[1]
-
-    # extend x1, x2
-#     x1_padding = np.ones((x1.shape[0], 3))
-#     x1_padding[:, :-1] = x1
-#     x2_padding = np.ones((x1.shape[0], 3))
-#     x2_padding[:, :-1] = x2
-
-    # normalize image coordinates
-    # x1 = x1 / x1[2]
-    # mean_1 = np.mean(x1[:2],axis=1)
-    # S1 = np.sqrt(2) / np.std(x1[:2])
-    # T1 = np.array([[S1,0,-S1*mean_1[0]],[0,S1,-S1*mean_1[1]],[0,0,1]])
-    # x1 = np.dot(T1,x1)
-#     T1 = np.array([[2/shape[1], 0, -1], [0, 2/shape[0], -1], [0, 0, 1]])
-#     x1_padding = np.dot(T1, x1_padding.T)
-#     x2_padding = np.dot(T1, x2_padding.T)
-
-    # compute F with the normalized coordinates
-    F = compute_fundamental(x1, x2)
-
-    # reverse normalization
-#     F = T1.T @ F @ T1
-
-#     return F/F[2,2]
     return F
 
 def compute_normalize_point(pt, matrix):
@@ -211,7 +140,9 @@ def RANSAC_fundamental(match_points_0, match_points_1, shape):
     max_inliers = []
     # max_inlier_h = None
     max_inlier_f = None
-    T1 = np.array([[2/shape[1], 0, -1], [0, 2/shape[0], -1], [0, 0, 1]])
+    T1 = np.array([[2/shape[1], 0, -1], 
+                   [0, 2/shape[0], -1],
+                   [0, 0, 1]])
     print("match_points_0.shape:")
 
     print(T1.shape)
@@ -220,7 +151,7 @@ def RANSAC_fundamental(match_points_0, match_points_1, shape):
 
     for i in range(1000):
         idx = np.random.randint(0, len(normal_p0), 60)
-        f = compute_fundamental_normalized(normal_p0[idx], normal_p1[idx], shape)
+        f = compute_fundamental(normal_p0[idx], normal_p1[idx])
 
         inliers = []
         for p0, p1 in zip(normal_p0, normal_p1):
@@ -243,33 +174,38 @@ def RANSAC_fundamental(match_points_0, match_points_1, shape):
 
 def compute_essential_candidate(f, K):
     E = K.T @ f @ K
-    U, S, V = np.linalg.svd(E)
+    U, S, VH = np.linalg.svd(E)
     S = np.diag(S)
     
     m = (S[1, 1] + S[2, 2]) / 2
-    E = U @ np.array([[m, 0, 0], [0, m, 0], [0, 0, 0]]) @ V.T
+    E = U @ np.array([[m, 0, 0], [0, m, 0], [0, 0, 0]]) @ VH
     
-    U, S, V = np.linalg.svd(E)
+    U, S, VH = np.linalg.svd(E)
     S = np.diag(S)
-    W = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+    W = np.array([[0, -1, 0],
+                  [1, 0, 0],
+                  [0, 0, 1]])
 
-    P1 = K @ np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
+    P1 = K @ np.array([[1, 0, 0, 0],
+                       [0, 1, 0, 0],
+                       [0, 0, 1, 0]])
+#     P1 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
 
     # P2 chooses
     P2_1 = np.zeros((3, 4))
-    P2_1[:, :-1] = U @ W @ V.T
+    P2_1[:, :-1] = U @ W @ VH
     P2_1[:, -1] = U[2]
 
     P2_2 = np.zeros((3, 4))
-    P2_2[:, :-1] = U @ W @ V.T
+    P2_2[:, :-1] = U @ W @ VH
     P2_2[:, -1] = (-1)*U[2]
 
     P2_3 = np.zeros((3, 4))
-    P2_3[:, :-1] = U @ W.T @ V.T
+    P2_3[:, :-1] = U @ W.T @ VH
     P2_3[:, -1] = U[2]
 
     P2_4 = np.zeros((3, 4))
-    P2_4[:, :-1] = U @ W.T @ V.T
+    P2_4[:, :-1] = U @ W.T @ VH
     P2_4[:, -1] = (-1)*U[2]
 
     P2_chooses = np.array([P2_1, P2_2, P2_3, P2_4])    
@@ -277,16 +213,18 @@ def compute_essential_candidate(f, K):
     return P1, P2_chooses
 
 
-def find_best_solution(x1, x2, P2_chooses, R, t):
+def find_best_solution(x1, x2, P2_chooses):
     ### add 1 for homogeneous ###
     x1_extend = np.ones((x1.shape[0], 3))
     x1_extend[:, :-1] = x1
     x2_extend = np.ones((x1.shape[0], 3))
     x2_extend[:, :-1] = x2
-
+    
     ### find best solution between each P2_chooses ###
     cnt_of_front_points = np.zeros(4)
     for i, P in enumerate(P2_chooses):
+        R = P[:, :-1]
+        t = P[:,-1].reshape(3,1)
         ### Create matrix A ###
         for point_idx in range(x1_extend.shape[0]): # for each [u,v] pair
             u_1, v_1 = x1_extend[point_idx, 0], x1_extend[point_idx, 1]
@@ -297,14 +235,17 @@ def find_best_solution(x1, x2, P2_chooses, R, t):
             P_3 = P[2].reshape((P[2].shape[0], 1))
             # print(P_1.shape)
             # print(u_1 @ P_0.T)
-            A = [u_1 * P_3.T - P_1.T, v_1 * P_3.T - P_2.T, u_2 * P_3.T - P_1.T, v_2 * P_3.T - P_2.T]
+            A = [u_1 * P_3.T - P_1.T,
+                 v_1 * P_3.T - P_2.T,
+                 u_2 * P_3.T - P_1.T,
+                 v_2 * P_3.T - P_2.T]
             A = np.array(A)
             A = A.reshape((4, 4))
             # print('A: ', A.shape)
 
             ### calculate X ###
-            U, S, V = np.linalg.svd(A)
-            X = V[:, -1]
+            U, S, VH = np.linalg.svd(A)
+            X = VH[-1]
             X = X / X[-1]
             X = X[:-1].reshape((3, 1))
             
@@ -324,108 +265,29 @@ def find_best_solution(x1, x2, P2_chooses, R, t):
 ##################### Triangulation ############################
 
 
-def Triangulation(x1, x2, P, R, t):
-    ### add 1 for homogeneous ###
-    x1_extend = np.ones((x1.shape[0], 3))
-    x1_extend[:, :-1] = x1
-    x2_extend = np.ones((x1.shape[0], 3))
-    x2_extend[:, :-1] = x2
+def Triangulation(x1, x2, P1, P2):
+    X = []
+    p1 = P1[0, :]
+    p2 = P1[1, :]
+    p3 = P1[2, :]
+    p1p = P2[0, :]
+    p2p = P2[1, :]
+    p3p = P2[2, :]
 
-    X_for_all = []
-    ### Calculate X matrix ###
-    for point_idx in range(x1_extend.shape[0]): # for each [u,v] pair
-        ### Create matrix A ###
-        u_1, v_1 = x1_extend[point_idx, 0], x1_extend[point_idx, 1]
-        u_2, v_2 = x2_extend[point_idx, 0], x2_extend[point_idx, 1]
-        # print(u_1.shape)
-        P_1 = P[0].reshape((P[0].shape[0], 1))
-        P_2 = P[1].reshape((P[1].shape[0], 1))
-        P_3 = P[2].reshape((P[2].shape[0], 1))
-        # print(P_1.shape)
-        # print(u_1 @ P_0.T)
-        A = [u_1 * P_3.T - P_1.T, v_1 * P_3.T - P_2.T, u_2 * P_3.T - P_1.T, v_2 * P_3.T - P_2.T]
-        A = np.array(A)
-        A = A.reshape((4, 4))
-        # print('A: ', A.shape)
+    for i in range(len(x1)):
+        u = x1[i, 0]
+        v = x1[i, 1]
+        up = x2[i, 0]
+        vp = x2[i, 1]
+        A = np.array([u  * p3 - p1,
+                      v  * p3 - p2,
+                      up * p3p - p1p,
+                      vp * p3p - p2p])
 
-        ### calculate X ###
-        U, S, V = np.linalg.svd(A)
-        X = V[:, -1]
-        X = X / X[-1]
-        X = X[:-1].reshape((3, 1))
+        U, S, VH = np.linalg.svd(A)
+        X.append((VH[-1] / VH[-1, -1])[:3])
 
-        X_for_all.append(X)
-    return np.array(X_for_all)
-
-##################### Texture Mapping For 3D model ##########################
-
-def fprintf(stream, format_spec, *args):
-    stream.write(format_spec % args)
-
-
-def CheckVisible(M, P1, P2, P3):
-    # used to check if the surface normal facing the camera
-    #  M: 3x4 projection matrix
-    #  P1, P2, P3: 3D points
-
-    tri_normal = np.cross((P2-P1), (P3-P2)) # surface normal of mesh
-
-    cam_dir = [M(3, 1), M(3, 2), M(3, 3)] # camera direction
-
-    if (dot(cam_dir, tri_normal) < 0):
-        bVisible = 1 # visible
-        # fprintf('Visible!\n');
-    else:
-        bVisible = 0 # invisible
-        # fprintf('inVisible!\n');
-    return bVisible
-
-'''
-def obj_main(P, p_img2, M, img, im_index):
-    img_size = img.shape
-
-    """ mesh-triangulation """
-    tri = Delaunay(p_img2) # 2D delaunay
-
-    # trisurf mesh triangulation
-
-    """ output .obj file """
-    # fid = fopen('model.obj', 'wt)
-    cmd_fid = ['fid = fopen(''model' str(im_index) '.obj'', ''wt'');']
-    eval(cmd_fid)
-    fprintf(fid, '# obj file\n')
-    cmd_print = ['fprintf(fid, ''mtllib model' str(im_index) '.mtl\n\n'');']
-    eval(cmd_print)
-    fprintf(fid, 'usemtl Texture\n')
-
-    # output 3D vertex informationo (3D points)
-    [len, dummy] = P.shape
-    for i in range(1, len):
-        fprintf(fid, 'v %f %f %f\n', P(i, 1), P(i,2), P(i,3))
-    fprintf(fid, '\n\n\n')
-
-    # output vertex texture coordinate (2D points)
-    for i in range(1, len):
-        # texture mapping
-        fprintf(fid, 'vt %f %f\n', p_img2(i, 1)/img_size(2), 1-p_img2(i, 2)/img_size(1))
-    fprintf(fid, '\n\n\n')
-
-    # output face information
-    [len_tri, dummy] = tri.shape
-    bVisible = 0
-    for i in range(1, len_tri):
-        # 3D mesh normal
-        # fprintf('loop %d \n',i)
-        bVisible = CheckVisible(M, P(tri(i,1), :).T, P(tri(i,2), :).T, P(tri(i,3), :).T)
-        if bVisible == 1:
-            fprintf(fid, 'f %d/%d %d/%d %d/%d\n', \
-                tri(i, 1), tri(i, 1), tri(i, 2), tri(i, 2), tri(i, 3), tri(i, 3))
-        else:
-            fprintf(fid, 'f %d/%d %d/%d %d/%d\n', \
-                tri(i, 2), tri(i, 2), tri(i, 1), tri(i, 1), tri(i, 3), tri(i, 3))
-    
-    fclose(fid)
-'''
+    return np.array(X)
 
 
 ##################### Main Driven Function ###############################
@@ -462,8 +324,8 @@ def sfm(img_0, img_1, K, R, t):
     match_points_1 = np.float32([kp_1[m.trainIdx].pt for m in good_matches]).reshape(-1, 2)
 
 #     print(gray_0.shape)
-    print("match_points_1")
-    print(match_points_1)
+    print("match_points_0")
+    print(match_points_0)
 
     ### 2. Estimate fundamental matrix ###
     f, inliers = RANSAC_fundamental(match_points_0, match_points_1, gray_0.shape)
@@ -473,7 +335,7 @@ def sfm(img_0, img_1, K, R, t):
     ### 3. Drlines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
     lines1 = cv2.computeCorrespondEpilines(match_points_1.reshape(-1,1,2), 2, f)
     lines1 = lines1.reshape(-1,3)
-    img5,img6 = drawlines(gray_0, gray_1, lines1, match_points_1, match_points_1)
+    img5,img6 = drawlines(gray_0, gray_1, lines1, match_points_0, match_points_1)
     plt.imshow(img5)
     plt.show()
 
@@ -482,12 +344,12 @@ def sfm(img_0, img_1, K, R, t):
     print('P2 chooses: ', P2_chooses)
 
     ### 5. Find out most appropriate solution ###
-    P2 = find_best_solution(match_points_0, match_points_1, P2_chooses, R, t)
+    P2 = find_best_solution(match_points_0, match_points_1, P2_chooses)
     print("P2:")
     print(P2)
 
     ### 6. Triangulation ###
-    X = Triangulation(match_points_0, match_points_1, P2, R, t)
+    X = Triangulation(match_points_0, match_points_1, P1, P2)
     print('X: ', X.shape)
     print('X: ', X)
 
